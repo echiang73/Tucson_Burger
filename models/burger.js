8267a8a6f18f9cb4b369e81d3477310d862392be
Neo <echiang73@gmail.com>
2019-05-02 12:35:56 -0700

    Removed GoogleTTS module

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,5 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
-var googleTTS = require('google-tts-api');
 
 var burger = {
   all: function(cb) {
