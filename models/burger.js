6e22df3321040fcc7b4f532ac2b4dcc6526e7bfa
Neo <echiang73@gmail.com>
2019-05-02 11:06:52 -0700

    Clean up order of code for place order and pickup

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,6 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
+var googleTTS = require('google-tts-api');
 
 var burger = {
   all: function(cb) {
