d401a3a9c43bcce26e00df86db6be02c78cf824c
Neo <echiang73@gmail.com>
2019-05-02 09:11:56 -0700

    Put gradient color in cards and added shadow to text

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@ INSERT INTO burgers (burger_name) VALUES ("Old Pueblo Burger");
 INSERT INTO burgers (burger_name) VALUES ("Tucson Heat Burger");
 INSERT INTO burgers (burger_name) VALUES ("Wildcat Whopper");
 INSERT INTO burgers (burger_name) VALUES ("Valley Fever Burger");
-INSERT INTO burgers (burger_name) VALUES ("Juicy Lucy");
+INSERT INTO burgers (burger_name) VALUES ("Juicy Lucy Burger");
